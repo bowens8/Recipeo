8cad2d30643b2bbb913563e0a951e39ab285c5ca
bowens8 <bowensjr8@gmail.com>
2026-07-28 16:19:36 -0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -9,12 +9,12 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-aut
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyAWjegirobVR-7bTr1VMP7O3zk_D0WRWGE",
+  authDomain: "recipeo-bdf64.firebaseapp.com",
+  projectId: "recipeo-bdf64",
+  storageBucket: "recipeo-bdf64.firebasestorage.app",
+  messagingSenderId: "266605426171",
+  appId: "1:266605426171:web:6f673d07b8429d36b1075e"
 };
 
 export const firebaseApp = initializeApp(firebaseConfig);
